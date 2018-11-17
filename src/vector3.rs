be7b6d2fcfcbf94878ccf973631e326894f6c6b4
Daniele D'Orazio <d.dorazio96@gmail.com>
2018-11-17 02:27:14 +0100

    add vector3 tests

--- src/vector3.rs
+++ src/vector3.rs
@@ -10,10 +10,12 @@ pub struct Vector3 {
 }
 
 impl Vector3 {
+    /// Create a new Vector3 from the given coordinates
     pub fn new(x: f32, y: f32, z: f32) -> Self {
         Vector3 { x, y, z }
     }
 
+    /// Create a new Vector3 with every coordinate rounded
     pub fn round(&self) -> Self {
         Vector3::new(self.x.round(), self.y.round(), self.z.round())
     }
@@ -100,5 +102,104 @@ impl MulAssign<f32> for Vector3 {
         self.x *= f;
         self.y *= f;
         self.z *= f;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::f32::consts::PI;
+
+    use super::Vector3;
+
+    #[test]
+    fn test_round() {
+        assert_eq!(
+            Vector3::new(0.3, -0.9, 2.9).round(),
+            Vector3::new(0.0, -1.0, 3.0)
+        );
+    }
+
+    #[test]
+    fn test_rotate_x() {
+        let mut v = Vector3::new(1.0, 2.0, 3.0);
+
+        v.rotate_x(0.0);
+        assert_eq!(v, Vector3::new(1.0, 2.0, 3.0));
+
+        v.rotate_x(PI / 2.0);
+        v.z = v.z.round();
+        assert_eq!(v, Vector3::new(1.0, -3.0, 2.0));
+
+        v.rotate_x(-PI / 2.0);
+        v.y = v.y.round();
+        assert_eq!(v, Vector3::new(1.0, 2.0, 3.0));
+
+        v.rotate_x(PI);
+        v.y = v.y.round();
+        v.z = v.z.round();
+        assert_eq!(v, Vector3::new(1.0, -2.0, -3.0));
+    }
+
+    #[test]
+    fn test_rotate_y() {
+        let mut v = Vector3::new(1.0, 2.0, 3.0);
+
+        v.rotate_y(0.0);
+        assert_eq!(v, Vector3::new(1.0, 2.0, 3.0));
+
+        v.rotate_y(PI / 2.0);
+        v.z = v.z.round();
+        assert_eq!(v, Vector3::new(3.0, 2.0, -1.0));
+
+        v.rotate_y(-PI / 2.0);
+        v.x = v.x.round();
+        assert_eq!(v, Vector3::new(1.0, 2.0, 3.0));
+
+        v.rotate_y(PI);
+        v.x = v.x.round();
+        v.z = v.z.round();
+        assert_eq!(v, Vector3::new(-1.0, 2.0, -3.0));
+    }
+
+    #[test]
+    fn test_rotate_z() {
+        let mut v = Vector3::new(1.0, 2.0, 3.0);
+
+        v.rotate_z(0.0);
+        assert_eq!(v, Vector3::new(1.0, 2.0, 3.0));
+
+        v.rotate_z(PI / 2.0);
+        v.y = v.y.round();
+        assert_eq!(v, Vector3::new(-2.0, 1.0, 3.0));
+
+        v.rotate_z(-PI / 2.0);
+        v.x = v.x.round();
+        assert_eq!(v, Vector3::new(1.0, 2.0, 3.0));
+
+        v.rotate_z(PI);
+        v.x = v.x.round();
+        v.y = v.y.round();
+        assert_eq!(v, Vector3::new(-1.0, -2.0, 3.0));
+    }
+
+    #[test]
+    fn test_basic_arithmetic() {
+        let mut v = Vector3::new(0.0, 2.0, 3.0);
+        assert_eq!(
+            v + Vector3::new(2.0, 42.0, 0.0),
+            Vector3::new(2.0, 44.0, 3.0)
+        );
+
+        v += Vector3::new(-2.0, 0.0, 0.0);
+        assert_eq!(v, Vector3::new(-2.0, 2.0, 3.0));
+
+        assert_eq!(v - v, Vector3::new(0.0, 0.0, 0.0));
+
+        assert_eq!(v * 3.0, Vector3::new(-6.0, 6.0, 9.0));
+
+        v *= 3.0;
+        assert_eq!(v, Vector3::new(-6.0, 6.0, 9.0));
+
+        assert_eq!(v / 3.0, Vector3::new(-2.0, 2.0, 3.0));
     }
 }
