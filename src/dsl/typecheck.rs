2cdbb1358dfa56f10cf5fd083572f8104b54a88c
Daniele D'Orazio <d.dorazio96@gmail.com>
2018-11-25 18:39:45 +0100

    dsl/typecheck: add simple tests

--- src/dsl/typecheck.rs
+++ src/dsl/typecheck.rs
@@ -55,3 +55,58 @@ impl<'input> std::fmt::Display for TypeCheckErrorKind<'input> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::dsl::parse_module;
+
+    #[test]
+    fn test_type_check() {
+        let prog = parse_module(
+            r"vertex v1 = 2 3 4
+              vertex v2 = 3 3 3
+              line v1 v2
+              vertex v3 = 0 0 0
+              triangle v1 v2 v3",
+        )
+        .unwrap();
+
+        assert_eq!(type_check(&prog), Ok(()));
+    }
+
+    #[test]
+    fn test_undeclared_var() {
+        let prog = parse_module(
+            r"vertex v1 = 2 3 4
+              line v1 v2",
+        )
+        .unwrap();
+
+        assert_eq!(
+            type_check(&prog),
+            Err(ast::Error {
+                line_no: 1,
+                line: "              line v1 v2",
+                kind: TypeCheckErrorKind::UndeclaredVariable("v2")
+            })
+        );
+
+        let prog = parse_module(
+            r"vertex v1 = 2 3 4
+              vertex v2 = 1 1 1
+              triangle v0 v1 v2",
+        )
+        .unwrap();
+
+        assert_eq!(
+            type_check(&prog),
+            Err(ast::Error {
+                line_no: 2,
+                line: "              triangle v0 v1 v2",
+                kind: TypeCheckErrorKind::UndeclaredVariable("v0")
+            })
+        );
+    }
+}
